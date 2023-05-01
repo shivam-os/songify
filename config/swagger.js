f2e10d9e4b98dedc5df9ccd4db557f58e5e952b8
Shivam <shivam89raj45@gmail.com>
2023-05-01 14:32:34 +0530

    Add Swagger CSS CDN for vercel to load CSS

--- config/swagger.js
+++ config/swagger.js
@@ -4,7 +4,11 @@ const path = require("path");
 const file = path.join(process.cwd(), "docs", "apiDocs.yaml");
 const swaggerJsDoc = yamljs.load(file);
 
+//CSS CDN for vercel
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.min.css";
+
 module.exports = {
   swaggerServe: swaggerUi.serve,
-  swaggerSetup: swaggerUi.setup(swaggerJsDoc),
+  swaggerSetup: swaggerUi.setup(swaggerJsDoc, { customCSSUrl: CSS_URL }),
 };
